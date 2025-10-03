e3d4732e03ed5b5163119f6c96f8472c9243cb77
TinselMoon <vitorcolatusso@gmail.com>
2025-10-02 21:58:39 -0300

    working

--- crivo.c
+++ crivo.c
@@ -2,17 +2,22 @@
 #include "list.h"
 
 void crivo (int n) { 
-  int i, sq = sqrt(n);       	
+  int i, sq = (int)sqrt(n), ref;
   List *l = create ();
   for(i = 2; i <= n; i++){
     l = insert_back(l, i);
   }
-  /*for(i = 2; i < n/2; i++){
-    List *tmp = l;
-    while(tmp != NULL){
-      tmp = 
-    }
-  }*/
+    List *item = l;
+  for(i = 2; i <= sq; i++){
+        ref = item->data;
+        for(List *atual = item->next; atual != NULL; atual = atual->next){
+            if(!(atual->data % ref)){
+                atual = atual->prev;
+                remove_queue(l, atual->next->data);
+            }
+        }
+        item = item->next;
+  }
   print(l);
   destroy (l);
 }   
@@ -21,4 +26,4 @@ int main () {
   int n = 30;
   crivo (n);
   return 0;
-} 
+}
